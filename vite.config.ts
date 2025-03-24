c258908abe38b2bf1bc686c0ac7188eddd6036ea
sooyaya <14344583+sooyaya@user.noreply.gitee.com>
2025-03-24 10:56:18 +0800

    Update base path in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/wenjuan/',
   plugins: [vue(), vueJsx(), vueDevTools()],
   resolve: {
     alias: {
